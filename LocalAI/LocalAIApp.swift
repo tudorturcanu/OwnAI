bbbb69536ce8ddb8d50d2eab265befd672889e78
Alice <alice.turcanu91@gmail.com>
2026-01-29 12:00:27 +0100

    Initial Commit

---
+++ LocalAI/LocalAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAIApp.swift
+//  LocalAI
+//
+//  Created by Tudor on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct LocalAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
